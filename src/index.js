28fe011e30674a8c00a7e1b2e09b086a81ee4bdf
marthigk <marthigk@ntnu.no>
2021-12-24 22:33:09 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 //<script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
 
 ReactDOM.render(
@@ -16,7 +16,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// reportWebVitals();
